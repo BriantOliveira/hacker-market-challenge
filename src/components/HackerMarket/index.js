bffb92ca4a659ddb899bfd1af0d73ece884c0895
Elliot Briant <elliot@paysplit.io>
2025-05-09 14:08:14 -0400

    adding toast popup

--- src/components/HackerMarket/index.js
+++ src/components/HackerMarket/index.js
@@ -1,4 +1,3 @@
-// src/components/HackerMart/index.js
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "../Navbar";
@@ -8,15 +7,24 @@ import products from "../../data/products";
 
 function HackerMart() {
   const [cart, setCart] = useState([]);
+  const [toastMessage, setToastMessage] = useState("");
 
   const addToCart = (product) => {
     if (!(cart ?? []).find(item => item.id === product.id)) {
       setCart([...(cart ?? []), product]);
+      showToast(`${product.name} added to cart`);
     }
   };
 
   const removeFromCart = (id) => {
-    setCart((cart ?? []).filter(item => item.id !== id));
+    const removed = cart.find(item => item.id === id);
+    setCart(cart.filter(item => item.id !== id));
+    showToast(`${removed?.name ?? "Item"} removed from cart`);
+  };
+
+  const showToast = (message) => {
+    setToastMessage(message);
+    setTimeout(() => setToastMessage(""), 2500);
   };
 
   return (
@@ -26,6 +34,12 @@ function HackerMart() {
         <Route path="/" element={<Home products={products} addToCart={addToCart} />} />
         <Route path="/cart" element={<Cart cart={cart} removeFromCart={removeFromCart} />} />
       </Routes>
+
+      {toastMessage && (
+        <div className="toast">
+          {toastMessage}
+        </div>
+      )}
     </Router>
   );
 }
