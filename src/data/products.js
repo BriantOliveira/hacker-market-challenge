105415c57e21531d20663735be622e4ceba0b5de
Elliot Briant <elliot@paysplit.io>
2025-05-09 15:15:42 -0400

    modifying products data

--- src/data/products.js
+++ src/data/products.js
@@ -1,7 +1,7 @@
 const products = [
-    { id: 1, name: "Keyboard", price: 49.99, description: "Mechanical keyboard" },
-    { id: 2, name: "Mouse", price: 19.99, description: "Wireless mouse" },
-    { id: 3, name: "Monitor", price: 149.99, description: "24-inch HD monitor" },
+    { id: 1, name: "Keyboard", price: 49.99, description: "Mechanical keyboard", image: 'https://t3.ftcdn.net/jpg/02/37/70/96/360_F_237709628_GbyhqnU6MGLT2JdgvFEzZ8sD61fkXNNe.jpg', },
+    { id: 2, name: "Mouse", price: 19.99, description: "Wireless mouse", image: 'https://www.freevector.com/uploads/vector/preview/10582/FreeVector-Computer-Mouse.jpg' },
+    { id: 3, name: "Monitor", price: 149.99, description: "24-inch HD monitor", image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwfHr1rD4YOAgZnKcSEMKmIulhcp2BDFJx6Q&s' },
   ];
   
   export default products;
